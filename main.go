4d6498ae01d9308e8bd5c82043bf61f857de32de
fbnoi <fbnoi@foxmail.com>
2020-12-29 22:21:38 +0800

    分离文件，添加routegroup

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"regexp"
 )
 
 type mux struct {
@@ -12,31 +12,14 @@ type mux struct {
 type routes []*route
 
 type route struct {
-	Path    string
+	regex   *regexp.Regexp
+	params  map[int]string
 	Handler http.HandlerFunc
 }
 
-func (m *mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, route := range m.router {
-		if route.Path == r.RequestURI {
-			route.Handler(w, r)
-			return
-		}
-	}
-	log.Print("404 not found")
-}
-
-func (m *mux) Handle(path string, handler http.HandlerFunc) {
-	m.router = append(m.router, &route{
-		Path:    path,
-		Handler: handler,
-	})
-}
-
 func main() {
-	m := &mux{}
-	m.Handle("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Print("hello world")
-	})
-	log.Fatal(http.ListenAndServe(":80", m))
+	// m := &mux{}
+	// m.Handle("/test/:id(\\d+)", func(w http.ResponseWriter, r *http.Request) {
+	// })
+	// log.Fatal(http.ListenAndServe(":80", m))
 }
---
+++ net/http/linac/router.go
@@ -0,0 +1,80 @@
+package linac
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+// Router model
+type Router struct {
+	routes []*Route
+}
+
+// Handle http
+func (router *Router) Handle(pattern string, handler http.HandlerFunc) {
+	parts := strings.Split(pattern, "/")
+	j := 0
+	params := make(map[int]string)
+	for i, part := range parts {
+		if strings.HasPrefix(part, ":") {
+			expr := "([^/]+)"
+
+			// a user may choose to override the defult expression
+			// similar to expressjs: ‘/user/:id([0-9]+)’
+			if index := strings.Index(part, "("); index != -1 {
+				expr = part[index:]
+				part = part[:index]
+			}
+			params[j] = part[1:]
+			parts[i] = expr
+			j++
+		}
+	}
+	pattern = strings.Join(parts, "/")
+	log.Print(pattern)
+	regex, regexErr := regexp.Compile(pattern)
+	if regexErr != nil {
+		panic(regexErr)
+	}
+	router.routes = append(router.routes, &Route{
+		Regex:   regex,
+		Params:  params,
+		Handler: handler,
+	})
+}
+
+// ServeHTTP ServeHTTP
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range router.routes {
+		if !route.Regex.MatchString(r.RequestURI) {
+			continue
+		}
+		matches := route.Regex.FindStringSubmatch(r.RequestURI)
+
+		//double check that the Route matches the URL pattern.
+		if len(matches[0]) != len(r.RequestURI) {
+			continue
+		}
+
+		params := make(map[string]string)
+
+		if len(route.Params) > 0 {
+			//add url parameters to the query param map
+			values := r.URL.Query()
+			for i, match := range matches[1:] {
+				values.Add(route.Params[i], match)
+				params[route.Params[i]] = match
+				log.Print(match)
+				log.Print(route.Params[i])
+			}
+			log.Print(params)
+			//reassemble query params and add to RawQuery
+			r.URL.RawQuery = url.Values(values).Encode() + "&" + r.URL.RawQuery
+			//r.URL.RawQuery = url.Values(values).Encode()
+		}
+		route.Handler(w, r)
+	}
+}
