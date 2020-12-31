9dd13f88360f4af7ea29eb5fa5e2446b34b8feec
fbnoi <fbnoi@foxmail.com>
2020-12-31 16:15:11 +0800

    添加 GET POST DELETE PUT HEAD http路由注册方法

--- net/http/linac/router.go
+++ net/http/linac/router.go
@@ -13,9 +13,34 @@ type Router struct {
 }
 
 // AddRoute 向路由器中添加路由
-func (router *Router) AddRoute(path, method string, handler Handler) *Router {
+func (router *Router) addRoute(path, method string, handler Handler) *Router {
 	router.engine.addRoute(path, method, handler)
 	return router
+}
+
+// GET 为一个路由注册一个GET方法
+func (router *Router) GET(path string, handler Handler) *Router {
+	return router.addRoute(path, "GET", handler)
+}
+
+// POST 为一个路由注册一个POST方法
+func (router *Router) POST(path string, handler Handler) *Router {
+	return router.addRoute(path, "POST", handler)
+}
+
+// PUT 为一个路由注册一个PUT方法
+func (router *Router) PUT(path string, handler Handler) *Router {
+	return router.addRoute(path, "PUT", handler)
+}
+
+// DELETE 为一个路由注册一个DELETE方法
+func (router *Router) DELETE(path string, handler Handler) *Router {
+	return router.addRoute(path, "DELETE", handler)
+}
+
+// HEAD 为一个路由注册一个HEAD方法
+func (router *Router) HEAD(path string, handler Handler) *Router {
+	return router.addRoute(path, "HEAD", handler)
 }
 
 // handleFunc 添加路由处理方法
@@ -27,19 +52,17 @@ func (router *Router) handleFunc(pattern, method string, handler http.HandlerFun
 	router.routes = append(router.routes, newRoute(pattern, method, handler))
 }
 
-// ServeHTTP ServeHTTP
+// ServeHTTP 响应http请求
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, route := range router.routes {
 		if !route.Regex.MatchString(r.RequestURI) {
 			continue
 		}
 		matches := route.Regex.FindStringSubmatch(r.RequestURI)
-
 		//double check that the Route matches the URL pattern.
 		if len(matches[0]) != len(r.RequestURI) {
 			continue
 		}
-
 		params := make(map[string]string)
 		if len(route.Params) > 0 {
 			//add url parameters to the query param map
