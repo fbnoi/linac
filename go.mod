9a891836de459ecdaf51e7f94c506d81f77b9707
fbnoi <fbnoi@foxmail.com>
2021-01-05 10:38:30 +0800

    添加

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module linac
 
 go 1.15
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/common v0.15.0
+)
---
+++ net/http/linac/recovery.go
@@ -0,0 +1,31 @@
+package linac
+
+import (
+	"fmt"
+	"net/http/httputil"
+	"os"
+	"runtime"
+
+	"github.com/prometheus/common/log"
+)
+
+// Recovery 从 panic 中恢复 server，并记录下 ctx
+func Recovery() Handler {
+	return func(c *Context) {
+		defer func() {
+			var rawReq []byte
+			if err := recover(); err != nil {
+				const size = 64 << 10
+				buf := make([]byte, size)
+				buf = buf[:runtime.Stack(buf, false)]
+				if c.Request != nil {
+					rawReq, _ = httputil.DumpRequest(c.Request, false)
+				}
+				pl := fmt.Sprintf("http call panic: %s\n%v\n%s\n", string(rawReq), err, buf)
+				fmt.Fprintf(os.Stderr, pl)
+				log.Error(pl)
+				c.Abort(500)
+			}
+		}()
+	}
+}
