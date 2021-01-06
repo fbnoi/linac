bdcc17d9f83b93a0849edabb8b19b72b4b69d57c
fbnoi <fbnoi@foxmail.com>
2021-01-06 15:42:29 +0800

    添加config，添加config.env

---
+++ config/env/env.go
@@ -0,0 +1,74 @@
+package env
+
+import (
+	"flag"
+	"os"
+)
+
+// 几种部署环境.
+const (
+	DeployEnvDev  = "dev"
+	DeployEnvTEST = "test"
+	DeployEnvProd = "prod"
+)
+
+// env 默认值
+const (
+	_defaultRegion    = "wuhan"
+	_defaultZone      = "wh001"
+	_defaultDeployEnv = DeployEnvDev
+)
+
+// env 环境配置.
+var (
+	// Region 地区
+	Region string
+	// Zone 可用域
+	Zone string
+	// Hostname 主机名
+	Hostname string
+	// DeployEnv 部署环境
+	DeployEnv string
+	// IP 服务IP
+	IP string
+	// AppID 服务ip
+	AppID string
+)
+
+// app 默认值
+var (
+	_defaultHTTPPort = "8080"
+)
+
+// app 配置项.
+var (
+	// HTTPPort app listen http port.
+	HTTPPort string
+)
+
+func init() {
+	var err error
+	if Hostname, err = os.Hostname(); err != nil || Hostname == "" {
+		Hostname = os.Getenv("HOSTNAME")
+	}
+	IP = os.Getenv("PODIP")
+}
+
+func addFlag(fs *flag.FlagSet) {
+	// env
+	fs.StringVar(&Region, "region", defaultString("REGION", _defaultRegion), "avaliable region. or use REGION env variable, value: wuhan etc.")
+	fs.StringVar(&Zone, "zone", defaultString("ZONE", _defaultZone), "avaliable zone. or use ZONE env variable, value: wh001/wh002 etc.")
+	fs.StringVar(&DeployEnv, "deploy.env", defaultString("DEPLOY_ENV", _defaultDeployEnv), "deploy env. or use DEPLOY_ENV env variable, value: dev/test/prod etc.")
+	fs.StringVar(&AppID, "appid", os.Getenv("APP_ID"), "appid is global unique application id, register by service tree. or use APP_ID env variable.")
+
+	// app
+	fs.StringVar(&HTTPPort, "http.port", defaultString("DISCOVERY_HTTP_PORT", _defaultHTTPPort), "app listen http port, default: 8080")
+}
+
+func defaultString(env, value string) string {
+	v := os.Getenv(env)
+	if v == "" {
+		return value
+	}
+	return v
+}
---
+++ config/env/env_test.go
@@ -0,0 +1,83 @@
+package env
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDefaultString(t *testing.T) {
+	v := defaultString("a", "test")
+	if v != "test" {
+		t.Fatal("v must be test")
+	}
+
+	if err := os.Setenv("a", "test_1"); err != nil {
+		t.Fatal(err)
+	}
+
+	v = defaultString("a", "test")
+	if v != "test_1" {
+		t.Fatal("v must be test_1")
+	}
+}
+
+func TestEnv(t *testing.T) {
+	cases := []struct {
+		flag string
+		env  string
+		def  string
+		val  *string
+	}{
+		{"region", "REGION", _defaultRegion, &Region},
+		{"zone", "ZONE", _defaultZone, &Zone},
+		{"deploy.env", "DEPLOY_ENV", _defaultDeployEnv, &DeployEnv},
+		{"appid", "APP_ID", "", &AppID},
+		{"http.port", "DISCOVERY_HTTP_PORT", _defaultHTTPPort, &HTTPPort},
+	}
+	for _, tc := range cases {
+		// flag set
+		t.Run(fmt.Sprintf("%s: flag set", tc.env), func(t *testing.T) {
+			fs := flag.NewFlagSet("", flag.ContinueOnError)
+			addFlag(fs)
+			err := fs.Parse([]string{fmt.Sprintf("-%s=%s", tc.flag, "test")})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if *tc.val != "test" {
+				t.Fatal("val must be test")
+			}
+		})
+
+		// flag not set, env set
+		t.Run(fmt.Sprintf("%s: flag not set, env set", tc.env), func(t *testing.T) {
+			*tc.val = ""
+			os.Setenv(tc.env, "test2")
+			fs := flag.NewFlagSet("", flag.ContinueOnError)
+			addFlag(fs)
+			err := fs.Parse([]string{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if *tc.val != "test2" {
+				t.Fatal("val must be test2")
+			}
+		})
+
+		// flag not set, env not set
+		t.Run(fmt.Sprintf("%s: flag not set, env not set", tc.env), func(t *testing.T) {
+			*tc.val = ""
+			os.Setenv(tc.env, "")
+			fs := flag.NewFlagSet("", flag.ContinueOnError)
+			addFlag(fs)
+			err := fs.Parse([]string{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if *tc.val != tc.def {
+				t.Fatal("val must be test")
+			}
+		})
+	}
+}
