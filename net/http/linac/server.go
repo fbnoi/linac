8b41d36b6e353164ac3886aa4b0eba1f274ee7ce
fbnoi <fbnoi@foxmail.com>
2021-01-13 09:51:42 +0800

    server engine添加配置

--- net/http/linac/server.go
+++ net/http/linac/server.go
@@ -1,9 +1,19 @@
 package linac
 
 import (
+	"errors"
 	"net/http"
 	"sync/atomic"
+	"time"
 )
+
+// ServerConfig 服务器配置
+type ServerConfig struct {
+	Address      string
+	Timeout      time.Duration
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
 
 // NewEngine 返回一个新的 http server engine
 func NewEngine() *Engine {
@@ -20,17 +30,36 @@ func NewEngine() *Engine {
 type Engine struct {
 	*Router
 	server *atomic.Value
+
+	config *atomic.Value
+}
+
+// SetConfig 设置服务器配置
+func (engine *Engine) SetConfig(conf *ServerConfig) {
+	engine.config.Store(conf)
+}
+
+// GetConfig 获取服务器配置
+func (engine *Engine) GetConfig() (conf *ServerConfig, ok bool) {
+	conf, ok = engine.config.Load().(*ServerConfig)
+	return
 }
 
 // Run 运行 http server engine
 func (engine *Engine) Run(address string) {
-	serve := &http.Server{
-		Addr:    address,
-		Handler: engine.Router,
-	}
-	engine.server.Store(serve)
-	if err := serve.ListenAndServe(); err != nil {
-		panic(err)
+	if conf, ok := engine.GetConfig(); ok {
+		serve := &http.Server{
+			Addr:         address,
+			Handler:      engine.Router,
+			ReadTimeout:  conf.ReadTimeout,
+			WriteTimeout: conf.WriteTimeout,
+		}
+		engine.server.Store(serve)
+		if err := serve.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	} else {
+		panic(errors.New("server engine should have server config, get nil"))
 	}
 }
 
